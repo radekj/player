0257888ab61a085697aba41527f03084c46aaef1
Radek Jankiewicz <radoslaw.jankiewicz@gmail.com>
2014-03-04 22:51:25 +0100

    server module

---
+++ player/server.py
@@ -0,0 +1,8 @@
+def application(environ, start_response):
+    start_response('200 OK', [('content-type', 'text/html')])
+    return [b'<h1>Hello world</h1>']
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    print("Serving on http://localhost:4000...")
+    make_server('localhost', 4001, application).serve_forever()
