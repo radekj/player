2e65f9fbf41d327f7aaf0fd92bda3bb415690a90
Radek Jankiewicz <radoslaw.jankiewicz@gmail.com>
2014-03-05 22:47:33 +0100

    communication

---
+++ player/decision.py
@@ -0,0 +1,6 @@
+import random
+
+def make_decisom(params):
+    #import pdb;pdb.set_trace()
+    bet = random.choice([0, 1, 2, 3, 4, 5])
+    return bet
--- player/play.py
+++ player/play.py
@@ -1,8 +1,17 @@
+import json
+from urllib.parse import parse_qs
+from wsgiref.simple_server import make_server
+from player.decision import make_decisom
+
 def application(environ, start_response):
-    start_response('200 OK', [('content-type', 'text/html')])
-    return [b'<h1>Hello world</h1>']
+    start_response('200 OK', [('content-type', 'application/json')])
+    request_body_size = int(environ['CONTENT_LENGTH'])
+    request_body = environ['wsgi.input'].read(request_body_size)
+    params = json.loads(request_body.decode('utf-8'))
+    result = make_decisom(params)
+    response = json.dumps(result)
+    return [response.encode('utf-8')]
 
 def run_server():
-    from wsgiref.simple_server import make_server
     print("Serving on http://localhost:4000...")
-    make_server('localhost', 4001, application).serve_forever()
+    make_server('localhost', 4000, application).serve_forever()
