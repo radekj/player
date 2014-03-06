ff5aed6f0d00e703c6cd2d1202f2cc13cb6a93af
Radek Jankiewicz <radoslaw.jankiewicz@gmail.com>
2014-03-06 23:09:10 +0100

    example responses for testing purposes

--- player/decision.py
+++ player/decision.py
@@ -1,6 +1,16 @@
 import random
 
 def make_decisom(params):
-    #import pdb;pdb.set_trace()
-    bet = random.choice([0, 1, 2, 3, 4, 5])
+    return 0
+    min_bet = params.get('min')
+    max_bet = params.get('max')
+    bet = random.choice([0, 1, 2,  3, 4, 5])
+    if min_bet:
+        #bet = min_bet
+        bet = random.choice([min_bet, min_bet, min_bet + 2])
+    if max_bet:
+        #bet = max_bet
+        bet = min_bet
+        #bet = random.choice([0, max_bet, max_bet])
+    print(bet)
     return bet
